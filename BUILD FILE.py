08df13a419142c86381861bb40c09e45e714c962
Mr. Bungle <99687409+YYC-CDN@users.noreply.github.com>
2025-10-04 12:53:02 -0600

    This will build the .EXE
    This will build the exe file

---
+++ BUILD FILE.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 22:21:46 2025
+
+@author: MrBungle
+
+icon 3 is my custom scary Ronald. Use it. 5:26build time
+"""
+
+!pyinstaller --onefile --icon=mcd_icon3.ico mcdvoice_automator_V1.17.14.py
